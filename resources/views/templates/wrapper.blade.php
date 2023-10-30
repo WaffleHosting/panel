af5fdc2f2206d10fde2c131fdf7960a09f80c6ae
JirayuSrisawat-Github <contact@jirayu.pw>
2023-10-30 20:57:05 +0700

    Assets

--- resources/views/templates/wrapper.blade.php
+++ resources/views/templates/wrapper.blade.php
@@ -12,7 +12,7 @@
             <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
             <link rel="manifest" href="/favicons/manifest.json">
             <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#bc6e3c">
-            <link rel="shortcut icon" href="/assets/svgs/icon.svg">
+            <link rel="shortcut icon" href="/favicons/favicon.svg">
             <meta name="msapplication-config" content="/favicons/browserconfig.xml">
             <meta name="theme-color" content="#0e4688">
         @show
